5763f3be9d8f04d849e9b1bedef094422fbb46c2
madunstar <maulanammm@gmail.com>
2018-02-04 11:08:40 +0800

    selesai setup data master

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -1,47 +1,21 @@
-<footer class="sticky-footer">
-      <div class="container">
-        <div class="text-center">
-          <small>Copyright © Your Website 2018</small>
-        </div>
-      </div>
-    </footer>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-      <i class="fa fa-angle-up"></i>
-    </a>
-    <!-- Logout Modal-->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-          <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="login.html">Logout</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js')?>"></script>
-    <script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="<?php echo base_url('assets/vendor/jquery-easing/jquery.easing.min.js')?>"></script>
-    <!-- Page level plugin JavaScript-->
-    <script src="<?php echo base_url('assets/vendor/chart.js/Chart.min.js')?>"></script>
-    <script src="<?php echo base_url('assets/vendor/datatables/jquery.dataTables.js')?>"></script>
-    <script src="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.js')?>"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="<?php echo base_url('assets/js/sb-admin.min.js')?>"></script>
-    <!-- Custom scripts for this page-->
-    <script src="<?php echo base_url('assets/js/sb-admin-datatables.min.js')?>"></script>
-    <script src="<?php echo base_url('assets/js/sb-admin-charts.min.js')?>"></script>
-  </div>
+<script src="<?php echo base_url('assets/js/jquery.js');?>"></script>
+  <!-- Bootstrap -->
+  <script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
+  <!-- App -->
+  <script src="<?php echo base_url('assets/js/app.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/slimscroll/jquery.slimscroll.min.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/charts/easypiechart/jquery.easy-pie-chart.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/charts/sparkline/jquery.sparkline.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/charts/flot/jquery.flot.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/charts/flot/jquery.flot.tooltip.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/charts/flot/jquery.flot.spline.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/charts/flot/jquery.flot.pie.min.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/charts/flot/jquery.flot.resize.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/charts/flot/jquery.flot.grow.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/charts/flot/demo.js');?>"></script>
 
+  <script src="<?php echo base_url('assets/js/calendar/bootstrap_calendar.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/calendar/demo.js');?>"></script>
 
-</html>
+  <script src="<?php echo base_url('assets/js/sortable/jquery.sortable.js');?>"></script>
+  <script src="<?php echo base_url('assets/js/app.plugin.js');?>"></script>
---
+++ application/views/layouts/scale_nav_master.php
@@ -0,0 +1,409 @@
+<section class="vbox">
+  <header class="bg-dark header header-md navbar navbar-fixed-top-xs box-shadow">
+    <div class="navbar-header aside-md dk">
+      <a class="btn btn-link visible-xs" data-toggle="class:nav-off-screen" data-target="#nav">
+        <i class="fa fa-bars"></i>
+      </a>
+      <a href="index.html" class="navbar-brand">
+        <img src="images/logo.png" class="m-r-sm" alt="scale">
+        <span class="hidden-nav-xs" style="font-size:15px">Pesantren Darul Ilmi</span>
+      </a>
+      <a class="btn btn-link visible-xs" data-toggle="dropdown" data-target=".user">
+        <i class="fa fa-cog"></i>
+      </a>
+    </div>
+    <ul class="nav navbar-nav hidden-xs">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          <i class="i i-grid"></i>
+        </a>
+        <section class="dropdown-menu aside-lg bg-white on animated fadeInLeft">
+          <div class="row m-l-none m-r-none m-t m-b text-center">
+            <div class="col-xs-4">
+              <div class="padder-v">
+                <a href="#">
+                  <span class="m-b-xs block">
+                    <i class="i i-mail i-2x text-primary-lt"></i>
+                  </span>
+                  <small class="text-muted">Mailbox</small>
+                </a>
+              </div>
+            </div>
+            <div class="col-xs-4">
+              <div class="padder-v">
+                <a href="#">
+                  <span class="m-b-xs block">
+                    <i class="i i-calendar i-2x text-danger-lt"></i>
+                  </span>
+                  <small class="text-muted">Calendar</small>
+                </a>
+              </div>
+            </div>
+            <div class="col-xs-4">
+              <div class="padder-v">
+                <a href="#">
+                  <span class="m-b-xs block">
+                    <i class="i i-map i-2x text-success-lt"></i>
+                  </span>
+                  <small class="text-muted">Map</small>
+                </a>
+              </div>
+            </div>
+            <div class="col-xs-4">
+              <div class="padder-v">
+                <a href="#">
+                  <span class="m-b-xs block">
+                    <i class="i i-paperplane i-2x text-info-lt"></i>
+                  </span>
+                  <small class="text-muted">Trainning</small>
+                </a>
+              </div>
+            </div>
+            <div class="col-xs-4">
+              <div class="padder-v">
+                <a href="#">
+                  <span class="m-b-xs block">
+                    <i class="i i-images i-2x text-muted"></i>
+                  </span>
+                  <small class="text-muted">Photos</small>
+                </a>
+              </div>
+            </div>
+            <div class="col-xs-4">
+              <div class="padder-v">
+                <a href="#">
+                  <span class="m-b-xs block">
+                    <i class="i i-clock i-2x text-warning-lter"></i>
+                  </span>
+                  <small class="text-muted">Timeline</small>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </li>
+    </ul>
+    <form class="navbar-form navbar-left input-s-lg m-t m-l-n-xs hidden-xs" role="search">
+      <div class="form-group">
+        <div class="input-group">
+          <span class="input-group-btn">
+            <button type="submit" class="btn btn-sm bg-white b-white btn-icon"><i class="fa fa-search"></i></button>
+          </span>
+          <input type="text" class="form-control input-sm no-border" placeholder="Search apps, projects...">
+        </div>
+      </div>
+    </form>
+    <ul class="nav navbar-nav navbar-right m-n hidden-xs nav-user user">
+      <li class="hidden-xs">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          <i class="i i-chat3"></i>
+          <span class="badge badge-sm up bg-danger count">2</span>
+        </a>
+        <section class="dropdown-menu aside-xl animated flipInY">
+          <section class="panel bg-white">
+            <div class="panel-heading b-light bg-light">
+              <strong>You have <span class="count">2</span> notifications</strong>
+            </div>
+            <div class="list-group list-group-alt">
+              <a href="#" class="media list-group-item">
+                <span class="pull-left thumb-sm">
+                  <img src="images/a0.png" alt="..." class="img-circle">
+                </span>
+                <span class="media-body block m-b-none">
+                  Use awesome animate.css<br>
+                  <small class="text-muted">10 minutes ago</small>
+                </span>
+              </a>
+              <a href="#" class="media list-group-item">
+                <span class="media-body block m-b-none">
+                  1.0 initial released<br>
+                  <small class="text-muted">1 hour ago</small>
+                </span>
+              </a>
+            </div>
+            <div class="panel-footer text-sm">
+              <a href="#" class="pull-right"><i class="fa fa-cog"></i></a>
+              <a href="#notes" data-toggle="class:show animated fadeInRight">See all the notifications</a>
+            </div>
+          </section>
+        </section>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          <span class="thumb-sm avatar pull-left">
+            <img src="images/a0.png" alt="...">
+          </span>
+          John.Smith <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu animated fadeInRight">
+          <li>
+            <span class="arrow top"></span>
+            <a href="#">Settings</a>
+          </li>
+          <li>
+            <a href="profile.html">Profile</a>
+          </li>
+          <li>
+            <a href="#">
+              <span class="badge bg-danger pull-right">3</span>
+              Notifications
+            </a>
+          </li>
+          <li>
+            <a href="docs.html">Help</a>
+          </li>
+          <li class="divider"></li>
+          <li>
+            <a href="modal.lockme.html" data-toggle="ajaxModal" >Logout</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </header>
+  <section>
+    <section class="hbox stretch">
+      <!-- .aside -->
+      <aside class="bg-black aside-md hidden-print" id="nav">
+        <section class="vbox">
+          <section class="w-f scrollable">
+            <div class="slim-scroll" data-height="auto" data-disable-fade-out="true" data-distance="0" data-size="10px" data-railOpacity="0.2">
+
+
+
+              <!-- nav -->
+              <nav class="nav-primary hidden-xs">
+                <div class="text-muted text-sm hidden-nav-xs padder m-t-sm m-b-sm">Menu</div>
+                <ul class="nav nav-main" data-ride="collapse">
+                  <li  class="active">
+                    <a href="index.html" class="auto">
+                      <i class="fa fa-dashboard">
+                      </i>
+                      <span class="font-bold">Dashboard</span>
+                    </a>
+                  </li>
+                  <li >
+                    <a href="#" class="auto">
+                      <span class="pull-right text-muted">
+                        <i class="i i-circle-sm-o text"></i>
+                        <i class="i i-circle-sm text-active"></i>
+                      </span>
+
+                      <i class="fa fa-bars">
+                      </i>
+                      <span class="font-bold">Data Master</span>
+                    </a>
+                    <ul class="nav dk">
+                      <li >
+                        <a href="layout-color.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Santri</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="layout-hbox.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Guru</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="layout-boxed.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Staff</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="layout-fluid.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data kelas</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="layout-fluid.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Mata Pelajaran</span>
+                        </a>
+                      </li>
+
+                    </ul>
+                  </li>
+                  <li >
+                    <a href="#" class="auto">
+                      <span class="pull-right text-muted">
+                        <i class="i i-circle-sm-o text"></i>
+                        <i class="i i-circle-sm text-active"></i>
+                      </span>
+                      <i class="i i-file-check icon">
+                      </i>
+                      <span class="font-bold">Presensi</span>
+                    </a>
+                    <ul class="nav dk">
+                      <li >
+                        <a href="buttons.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Jadwal Pelajaran</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="icons.html" class="auto">
+                          <b class="badge bg-info pull-right">369</b>
+                          <i class="i i-dot"></i>
+
+                          <span>Presensi Siswa</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="grid.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Rekap Presensi Siswa</span>
+                        </a>
+                      </li>
+
+                    </ul>
+                  </li>
+                  <li >
+                    <a href="#" class="auto">
+                      <span class="pull-right text-muted">
+                        <i class="i i-circle-sm-o text"></i>
+                        <i class="i i-circle-sm text-active"></i>
+                      </span>
+                      <i class="fa fa-briefcase">
+                      </i>
+                      <span class="font-bold">Perizinan</span>
+                    </a>
+                    <ul class="nav dk">
+                      <li >
+                        <a href="profile.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Keluar Pondok</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="invoice.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Kembali ke Pondok</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="intro.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Denda</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="master.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Pembayaran Denda</span>
+                        </a>
+                      </li>
+
+                    </ul>
+                  </li>
+                  <li >
+                    <a href="#" class="auto">
+                      <span class="pull-right text-muted">
+                        <i class="i i-circle-sm-o text"></i>
+                        <i class="i i-circle-sm text-active"></i>
+                      </span>
+                      <i class="fa fa-cogs">
+                      </i>
+                      <span class="font-bold">Lain lain</span>
+                    </a>
+                    <ul class="nav dk">
+                      <li >
+                        <a href="mail.html" class="auto">
+
+                          <i class="i i-dot"></i>
+
+                          <span>Data Admin</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="fullcalendar.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Pendidikan</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="project.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Pekerjaan</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="media.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Provinsi</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="media.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Kota /Kabupaten</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="media.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Kecamatan</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="media.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Desa / Keluarahan</span>
+                        </a>
+                      </li>
+                      <li >
+                        <a href="media.html" class="auto">
+                          <i class="i i-dot"></i>
+
+                          <span>Data Role Admin</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li  class="">
+                    <a href="index.html" class="auto">
+                      <i class="fa fa-folder-open-o">
+                      </i>
+                      <span class="font-bold">Pendaftaran</span>
+                    </a>
+                  </li>
+                </ul>
+
+
+                
+              </nav>
+              <!-- / nav -->
+            </div>
+          </section>
+
+          <footer class="footer hidden-xs no-padder text-center-nav-xs">
+            <a href="modal.lockme.html" data-toggle="ajaxModal" class="btn btn-icon icon-muted btn-inactive pull-right m-l-xs m-r-xs hidden-nav-xs">
+              <i class="i i-logout"></i>
+            </a>
+            <a href="#nav" data-toggle="class:nav-xs" class="btn btn-icon icon-muted btn-inactive m-l-xs m-r-xs">
+              <i class="i i-circleleft text"></i>
+              <i class="i i-circleright text-active"></i>
+            </a>
+          </footer>
+        </section>
+      </aside>
+      <!-- /.aside -->
