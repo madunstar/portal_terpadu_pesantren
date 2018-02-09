2539adf6d146372215681ac93ddd06364a497ecb
madunstar <maulanammm@gmail.com>
2018-02-09 18:01:56 +0800

    menghapus yang tak perlu

--- application/views/layouts/content.php
+++
@@ -1,7 +0,0 @@
-<?php $this->load->view('layouts/header'); ?>
-
-<?php $this->load->view('layouts/scale_nav_master'); ?>
-
-    <?php 	if(isset($_view) && $_view)
-    $this->load->view($_view);
-    ?>
--- application/views/layouts/content_pendaftaran_register.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('layouts/header'); ?>
-
-<?php 	if(isset($_view) && $_view)
-$this->load->view($_view);
-?>
