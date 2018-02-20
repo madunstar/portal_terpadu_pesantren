67441dba6405a4f59221f6566c9a4ddd6f354fb6
madunstar <maulanammm@gmail.com>
2018-02-20 13:44:18 +0800

    lihat bukti bayar oleh admin

--- application/views/adminpendaftaran/verifikasi/v_pembayaran.php
+++ application/views/adminpendaftaran/verifikasi/v_pembayaran.php
@@ -4,14 +4,17 @@
     <div class="m-b-md">
       <h3 class="m-b-none">Santri</h3>
     </div>
+
     <section class="panel panel-default">
       <header class="panel-heading">
         List Santri
         <i class="fa fa-info-sign text-muted" data-toggle="tooltip" data-placement="bottom" data-title="ajax to load the data."></i>
       </header>
+      <div class="panel-body">
+
       <div class="table-responsive">
       <?php pesan_get('msg',"Berhasil Menghapus Data Santri","Gagal Menghapus Data Santri") ?>
-      
+
         <table class="table table-striped " id="datatable">
           <thead>
 
@@ -25,26 +28,34 @@
           <tbody>
             <?php
                 foreach($data->result_array() as $row){
-                  echo "
+                  ?>
                     <tr>
 
-                      <td>".$row['nama_lengkap']."</td>
-                      <td>".$row['besar_pembayaran']."</td>
-                      <td>".$row['tanggal_pembayaran']."</td>
-                      <td>".$row['status_pembayaran']."</td>
+                      <td><?php echo $row['nama_lengkap']?></td>
+                      <td><?php echo $row['besar_pembayaran']?></td>
+                      <td><?php echo $row['tanggal_pembayaran'] ?></td>
+                      <td><?php echo $row['status_pembayaran'] ?></td>
                       <td>
-                        <form action='".base_url()."admin/pendaftaran/verifikasibayar?email_pendaftar=".$row['email_pendaftar']."' method='post'>
-                        <button class='btn btn-success btn-sm'>verifikasi</button>
-                        </form>
+                        <div class="form-group">
+
+
+                          <form action='<?php echo base_url()?>admin/pendaftaran/verifikasibayar?email_pendaftar=<?php echo $row['email_pendaftar']?>' method='post'>
+                          <button type="submit" class='btn btn-success btn-xs'>verifikasi</button>
+                          <a class="" target='__blank' <?php echo ($row['status_pembayaran'] == 'tidak lengkap' ? '' : 'href="'.base_url('assets/images/berkas/'.$row['bukti_pembayaran']).'"');?>><button type="button" class="btn btn-warning btn-xs"><i class="fa fa-search"></i></button></a>
+                          </form>
+                        </div>
+
+
+
                       </td>
 
                     </tr>
-                  ";
-                }
-            ?>
+
+              <?php  } ?>
           </tbody>
         </table>
       </div>
+    </div>
     </section>
   </section>
 </section>
