eda2dc9c4e7c7861853d676c1de2a8094f65c865
Ilyas <ilyas.alyafii@gmail.com>
2018-02-24 23:06:31 +0800

    Login Mantap

--- application/libraries/Layout.php
+++ application/libraries/Layout.php
@@ -5,19 +5,29 @@ class Layout {
         $this->ci = & get_instance();;
     }
     public $header = 'back-end/template/header';
+    public $header_login = 'back-end/template/header_login';
 	//public $menu = 'template/menu';
     public $footer = 'back-end/template/footer';
     public $endhtml = 'back-end/template/endhtml';
-    
+
     public $headerfront = 'front-end/template/header';
 	//public $menu = 'template/menu';
     public $footerfront = 'front-end/template/footer';
 	public $endhtmlfront= 'front-end/template/endhtml';
-    
+
     function render($view,$data = null,$js = null){
         $this->ci->load->view($this->header);
         $this->ci->load->view($view,$data);
         $this->ci->load->view($this->footer);
+        if ($js!=NULL) {
+            $this->ci->load->view($js,$data);
+        }
+        $this->ci->load->view($this->endhtml);
+    }
+
+    function renderlogin($view,$data = null,$js = null){
+        $this->ci->load->view($this->header_login);
+        $this->ci->load->view($view,$data);
         if ($js!=NULL) {
             $this->ci->load->view($js,$data);
         }
