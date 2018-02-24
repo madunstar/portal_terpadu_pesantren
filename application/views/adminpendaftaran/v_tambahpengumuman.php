1ed22f9d8f1882b3be76a6b894dba7529481d61e
madunstar <maulanammm@gmail.com>
2018-02-24 16:33:35 +0800

    textarea

--- application/views/adminpendaftaran/v_tambahpengumuman.php
+++ application/views/adminpendaftaran/v_tambahpengumuman.php
@@ -87,8 +87,8 @@
                               <a class="btn btn-default btn-sm" data-edit="redo" title="Redo (Ctrl/Cmd+Y)"><i class="fa fa-repeat"></i></a>
                             </div>
                           </div>
-                          <div id="editor" class="form-control" style="overflow:scroll;height:150px;max-height:150px">
-
+                          <div>
+                            <textarea id="editor" class="form-control" style="overflow:scroll;height:150px;max-height:150px"></textarea>
                           </div>
                         </div>
                       </div>
