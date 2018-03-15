ac163420b4ca8d54775692323c1e699c1f4c7cbc
madunstar <maulanammm@gmail.com>
2018-03-15 14:22:52 +0800

    laporan denda

--- application/views/back-end/perizinan/v_denda.php
+++ application/views/back-end/perizinan/v_denda.php
@@ -12,6 +12,8 @@
           <section class="panel panel-default">
             <header class="panel-heading">
               <h4 class="font-bold">Data Denda Keterlambatan</h4>
+
+
             </header>
             <div class="panel-body">
               <div class="table-responsive">
@@ -44,7 +46,55 @@
                   </tbody>
                 </table>
               </div>
+              <div class="panel-footer">
+                <button type="button" data-toggle='modal' data-target='#cetaklap' name="button" class="btn btn-primary">Cetak Laporan <i class="fa fa-print"></i></button>
+                <div class='modal' id='cetaklap' tabindex='-1' role='dialog'>
+                  <form class='form-horizontal' role='form' data-validate='parsley' action='<?php echo base_url() ?>admin/perizinan/bayardenda' method='post'>
+                   <div class='modal-dialog' role='document'>
+                     <div class='modal-content'>
+                       <div class='modal-header bg-primary'>
+                         <h4 class='modal-title'>Cetak Laporan</h4>
+                       </div>
+                       <div class='modal-body'>
+                         <div class='form-group'>
+                           <div class="col-sm-6">
+                             <select type="text" class="form-control" name="bulan" >
+                               <option value="01">Januari</option>
+                               <option value="02">Februari</option>
+                               <option value="03">Maret</option>
+                               <option value="04">April</option>
+                               <option value="05">Mei</option>
+                               <option value="06">Juni</option>
+                               <option value="07">Juli</option>
+                               <option value="08">Agustus</option>
+                               <option value="09">September</option>
+                               <option value="10">Oktober</option>
+                               <option value="11">November</option>
+                               <option value="12">Desember</option>
+                            </select>
+                           </div>
+                           <div class="col-sm-6">
+                             <select class="form-control" name="tahun">
+                               <?php
+                                 for($i = 2000 ; $i <= date('Y'); $i++){ ?>
+                                  <option value='<?php echo $i ?>' <?php if ($i == date('Y')) echo 'selected' ?>><?php echo $i ?></option>
 
+                                <?php } ?>
+
+                             </select>
+
+                           </div>
+                         </div>
+                       <div class='modal-footer'>
+                         <button type='submit' class='btn btn-sm btn-success'>Cetak <i class="fa fa-check"></i></button>
+                         <button type='button' class='btn btn-sm btn-default' data-dismiss='modal'>Batal</button>
+                       </div>
+                     </div>
+                   </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
 
           </div>
