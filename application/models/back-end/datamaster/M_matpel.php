1ca0784c05a4a38d632c981c94d73508f46fd001
Nazaro <nazarbatuah@users.noreply.github.com>
2018-03-18 01:07:24 +0800

    Membuat CRUD Mata Pelajaran
    Sekaligus menyempurnakan CRUD kelas sebelumnya

---
+++ application/models/back-end/datamaster/M_matpel.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+Class M_matpel extends CI_Model {
+	function lihatdata()
+    {
+        return $this->db->get('tb_mata_pelajaran');
+    }
+
+    function lihatdatasatu($kode)
+    {
+        $this->db->where("kd_matpel",$kode);
+        return $this->db->get('tb_mata_pelajaran');
+    }
+
+    function tambahdata($array)
+    {
+        return $this->db->insert('tb_mata_pelajaran',$array);
+    }
+
+    function editdata($kode,$array)
+    {
+        $this->db->where("kd_matpel",$kode);
+        return $this->db->update('tb_mata_pelajaran',$array);
+    }
+    function hapus($kode)
+    {
+        $this->db->where("kd_matpel",$kode);
+        return $this->db->delete('tb_mata_pelajaran');
+    }
+}
