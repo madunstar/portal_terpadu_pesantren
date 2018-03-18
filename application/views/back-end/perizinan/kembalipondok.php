482f4e50d13a930fba058b8465f4471b1fe42273
madunstar <maulanammm@gmail.com>
2018-03-18 15:35:42 +0800

    membujuri pun anis

--- application/views/back-end/perizinan/kembalipondok.php
+++ application/views/back-end/perizinan/kembalipondok.php
@@ -19,7 +19,7 @@
                 <div class="form-group">
                   <label class="col-sm-2 control-label" for="input-id-1">Pilih Santri</label>
                   <div class="col-sm-8">
-                    <select name="id_santri" id="id_santri" class="form-control" data-required="true">
+                    <select name="id_santri" id="id_santri" class="form-control chosen-select" data-required="true">
                       <option value="">-PILIH SANTRI KELUAR-</option>
                       <?php foreach($santrikeluar->result_array() as $row):?>
                         <option value="<?php echo $row['nis_santri'];?>"><?php echo $row['nis_santri'];?></option>
@@ -45,7 +45,7 @@
                 <div class="form-group">
                       <label class="col-sm-2 control-label">Tanggal keluar</label>
                       <div class="col-sm-8">
-                        <input class="input-sm input-s datepicker-input form-control" size="16" type="text"  readonly data-date-format="dd-mm-yyyy" >
+                        <input class="input-sm input-s form-control" size="16" type="text"  readonly data-date-format="dd-mm-yyyy" >
                       </div>
                 </div>
                 <div class="form-group">
