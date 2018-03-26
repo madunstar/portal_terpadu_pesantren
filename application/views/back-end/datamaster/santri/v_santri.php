23e5b4e3426a4c8c064ce6c90d44667befeb10af
madunstar <maulanammm@gmail.com>
2018-03-26 10:23:51 +0800

    tampilan prestasi santri

--- application/views/back-end/datamaster/santri/v_santri.php
+++ application/views/back-end/datamaster/santri/v_santri.php
@@ -40,8 +40,8 @@
                       <td>".($row['jenis_kelamin']=="L"?"Laki-laki":"Perempuan")."</td>
                       <td>
                         <a href='".base_url('admin/datamaster/santriberkas?nis='.$row['nis_lokal'].'')."' class='btn btn-success btn-xs' title='Berkas'><i class='fa fa-file-text-o'></i></a>
-                        <a href='".base_url('admin/datamaster/prestasi')."' class='btn btn-primary btn-xs' title='Prestasi'><i class='fa fa-trophy'></i></a>
-                        <a href='".base_url('admin/datamaster/pelanggaran')."' class='btn btn-danger btn-xs' title='Pelanggaran'><i class='fa fa-ban'></i></a>
+                        <a href='".base_url('admin/datamaster/prestasisantri?nis='.$row['nis_lokal'].'')."' class='btn btn-primary btn-xs' title='Prestasi'><i class='fa fa-trophy'></i></a>
+                        <a href='".base_url('admin/datamaster/pelanggaran?nis='.$row['nis_lokal'].'')."' class='btn btn-danger btn-xs' title='Pelanggaran'><i class='fa fa-ban'></i></a>
                       </td>
                     </tr>
                   ";
--- application/views/back-end/prestasi_pelanggaran/v_data_prestasi.php
+++ application/views/back-end/prestasi_pelanggaran/v_data_prestasi.php
@@ -11,10 +11,11 @@
         <div class="col-sm-12">
           <section class="panel panel-default">
             <header class="panel-heading">
-              <h4 class="font-bold">Data Prestasi</h4>
+              <h4 class="font-bold">Data Prestasi <?php echo $santri['nama_lengkap']?> / <?php echo $santri['nis_lokal']?></h4>
             </header>
             <div class="panel-body">
             <a href="<?php echo base_url() ?>admin/perizinan/keluar"><button type="button" name="button" class="btn btn-success btn-rounded"><i class="fa fa-plus"></i> Tambah Prestasi</button></a>
+            <a style="margin: 10px 0 10px 0px" href="<?php echo base_url() ?>admin/datamaster/santri" class="btn btn-s-md btn-default" ><i class="fa fa-list"></i> List Santri</a>
               <div class="table-responsive">
                 <table class="table table-striped m-b-none" id="datatable">
                   <thead>
@@ -22,16 +23,28 @@
                       <th>Aksi</th>
                       <th >Prestasi</th>
                       <th >Tanggal Diperoleh</th>
-                      <th >Reward</th>
-                      <th>Berkas</th>
+                      <th >Jenis Prestasi</th>
+                      <th>Keterangan</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <td>button button button</td>
-                    <td>a</td>
-                    <td>a</td>
-                    <td>a</td>
-                    <td>a</td>
+                    <?php
+                        foreach($data->result_array() as $row){
+                          echo "
+                            <tr>
+                              <td>
+                              <a href='".base_url('admin/datamaster/santrilihat?nis='.$row['nis_lokal'].'')."' class='btn btn-primary btn-xs' title='Lihat'><i class='fa fa-eye'></i></a>
+                              <a href='".base_url('admin/datamaster/santriedit?nis='.$row['nis_lokal'].'')."' class='btn btn-warning btn-xs' title='Edit'><i class='fa fa-edit'></i></a>
+                              <a href='#' class='btn btn-danger btn-xs hapus' title='Hapus' id='".$row['nis_lokal']."'><i class='fa fa-trash-o'></i></a>
+                              </td>
+                              <td>".$row['prestasi']."</td>
+                              <td>".$row['tanggal_prestasi']."</td>
+                              <td>".$row['jenis_prestasi']."</td>
+                              <td>".$row['keterangan']."</td>
+                            </tr>
+                          ";
+                        }
+                    ?>
                   </tbody>
                 </table>
               </div>
