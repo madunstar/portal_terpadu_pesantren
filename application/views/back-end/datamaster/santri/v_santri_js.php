0cd669fb5a12541dbe075b06ec159395d267127b
M Ni'man Nasir <nikmannasir@outlook.com>
2018-06-02 17:05:24 +0800

    hanyar

--- application/views/back-end/datamaster/santri/v_santri_js.php
+++ application/views/back-end/datamaster/santri/v_santri_js.php
@@ -1,27 +1,95 @@
 <script>
- $(document).ready(function(){
-   $('#datatable').DataTable({});
-
-   $(".hapus").click(function (e) {
-    var v_nis = this.id;
-    $.confirm({
-        title: 'Hapus!',
-        content: 'Yakin ingin menghapus ?',
-        buttons: {
-            hapus: {
-                text: 'Hapus',
-                btnClass: 'btn-green',
-                action: function(){
-                    window.location.assign("<?php echo base_url() ?>admin/datamaster/santrihapus?nis="+v_nis);
-                }
-            },
-            batal: function () {
-
-            }
+   $('#datatable').DataTable({
+        "bStateSave"    : true,
+        "ajax"          : {
+                            url :"<?php echo base_url(); ?>admin/datamaster/santriajax", // json datasource
+                            type: "post",  // method  , by default get
+                            error: function(){  // error handling
+                                $(".employee-grid-error").html("");
+                                $("#employee-grid").append('<tbody class="employee-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
+                                $("#employee-grid_processing").css("display","none");
+                            }
+                        },
+        "processing"    : true,
+        "serverSide"    : true,
+        "columnDefs"    : [
+                            { "orderable": false, "targets": [0]
+                            }
+                        ],
+						
+		"responsive": true,
+        "fnDrawCallback": function(oSettings){
             
-        }
-        });
+            $(".hapus").click(function (e) {
+            var v_nis = this.id;
+            $.confirm({
+                title: 'Hapus!',
+                content: 'Yakin ingin menghapus ?',
+                buttons: {
+                    hapus: {
+                        text: 'Hapus',
+                        btnClass: 'btn-green',
+                        action: function(){
+                            window.location.assign("<?php echo base_url() ?>admin/datamaster/santrihapus?nis="+v_nis);
+                        }
+                    },
+                    batal: function () {
+
+                    }
+                    
+                }
+                });
+            });
+
+            $(".edit").click(function(e) {
+        var v_url = "<?php echo base_url() ?>admin/datamaster/santritingkat";
+        var v_id = this.id;
+        $.ajax({
+				type: 'POST',
+				url: v_url,
+				data: {
+                     nis : v_id
+                      },
+				beforeSend: function () {
+					$("#loading").show();
+				},
+				success: function (response) {
+					$("#loading").hide();
+					$('#modal-edit').html(response)
+				}
+            });
+       
     });
+
+    $(".edit2").click(function(e) {
+        var v_url = "<?php echo base_url() ?>admin/datamaster/santritingkatpondokan";
+        var v_id = this.id;
+        $.ajax({
+				type: 'POST',
+				url: v_url,
+				data: {
+                     nis : v_id
+                      },
+				beforeSend: function () {
+					$("#loading").show();
+				},
+				success: function (response) {
+					$("#loading").hide();
+					$('#modal-edit').html(response)
+				}
+            });
+       
+    });
+
+
+                
+        }
+   });
+
+
+ $(document).ready(function(){
+
+  
 
     $('#provinsi').change(function(){
         var id=$(this).val();
@@ -84,45 +152,6 @@
     });
 
 
-    $(".edit").click(function(e) {
-        var v_url = "<?php echo base_url() ?>admin/datamaster/santritingkat";
-        var v_id = this.id;
-        $.ajax({
-				type: 'POST',
-				url: v_url,
-				data: {
-                     nis : v_id
-                      },
-				beforeSend: function () {
-					$("#loading").show();
-				},
-				success: function (response) {
-					$("#loading").hide();
-					$('#modal-edit').html(response)
-				}
-            });
-       
-    });
-
-    $(".edit2").click(function(e) {
-        var v_url = "<?php echo base_url() ?>admin/datamaster/santritingkatpondokan";
-        var v_id = this.id;
-        $.ajax({
-				type: 'POST',
-				url: v_url,
-				data: {
-                     nis : v_id
-                      },
-				beforeSend: function () {
-					$("#loading").show();
-				},
-				success: function (response) {
-					$("#loading").hide();
-					$('#modal-edit').html(response)
-				}
-            });
-       
-    });
-
+    
 });
 </script>
