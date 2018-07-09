f3e320e1206e1c9604452e612cc8c619403ca022
madunstar <maulanammm@gmail.com>
2018-07-09 10:10:34 +0800

    mewarnai hidupmu

--- application/views/back-end/datamaster/jenjang/v_jenjang.php
+++ application/views/back-end/datamaster/jenjang/v_jenjang.php
@@ -28,8 +28,8 @@
                   echo "
                     <tr>
                       <td>
-                      <a href='".base_url('admin/datamaster/jenjangedit?jenjang='.$row['jenjang'].'')."' class='btn btn-success btn-xs' title='Edit'><i class='fa fa-edit'></i></a>
-                      <a href='#' class='btn btn-success btn-xs hapus' title='Hapus' id='".$row['jenjang']."'><i class='fa fa-trash-o'></i></a>
+                      <a href='".base_url('admin/datamaster/jenjangedit?jenjang='.$row['jenjang'].'')."' class='btn btn-warning btn-xs' title='Edit'><i class='fa fa-edit'></i></a>
+                      <a href='#' class='btn btn-danger btn-xs hapus' title='Hapus' id='".$row['jenjang']."'><i class='fa fa-trash-o'></i></a>
                       </td>
                       <td>".$row['jenjang']."</td>
                       <td>".$row['namajenjang']."</td>
--- application/views/back-end/datamaster/pondokan/v_pondokan.php
+++ application/views/back-end/datamaster/pondokan/v_pondokan.php
@@ -28,8 +28,8 @@
                   echo "
                     <tr>
                       <td>
-                      <a href='".base_url('admin/datamaster/pondokanedit?pondokan='.$row['pondokan'].'')."' class='btn btn-success btn-xs' title='Edit'><i class='fa fa-edit'></i></a>
-                      <a href='#' class='btn btn-success btn-xs hapus' title='Hapus' id='".$row['pondokan']."'><i class='fa fa-trash-o'></i></a>
+                      <a href='".base_url('admin/datamaster/pondokanedit?pondokan='.$row['pondokan'].'')."' class='btn btn-warning btn-xs' title='Edit'><i class='fa fa-edit'></i></a>
+                      <a href='#' class='btn btn-danger btn-xs hapus' title='Hapus' id='".$row['pondokan']."'><i class='fa fa-trash-o'></i></a>
                       </td>
                       <td>".$row['pondokan']."</td>
                       <td>".$row['namapondokan']."</td>
