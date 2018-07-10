062736340989581fe31dee8651ca56f24e0b3ba0
M Ni'man Nasir <nikman.mnn@gmail.com>
2018-07-10 12:04:57 +0800

    hnyar

--- application/models/back-end/datamaster/M_santri.php
+++ application/models/back-end/datamaster/M_santri.php
@@ -180,7 +180,7 @@ class M_santri extends CI_Model
       }
 
       function lihattingkatanpondokan($nis_lokal){
-        return $this->db->query("SELECT `tb_kelas_santri`.nis_lokal,`tb_presensi_pondokan`.`id_kelas_belajar`, `tb_presensi_pondokan`.`nama_kelas_belajar`, `tb_presensi_pondokan`.`pondokan`, `tb_presensi_pondokan`.`tingkat`, `tb_tahun_ajaran`.`tahun_ajaran` FROM `tb_kelas_santri` inner join `tb_presensi_pondokan` on `tb_presensi_pondokan`.`id_kelas_belajar`=`tb_kelas_santri`.`id_kelas_belajar` inner join `tb_tahun_ajaran` on `tb_tahun_ajaran`.`id_tahun`=`tb_presensi_pondokan`.`id_tahun` where `nis_lokal` = '$nis_lokal' order by `tb_presensi_pondokan`.`tingkat` asc ");
+        return $this->db->query("SELECT `tb_pondokan_santri`.nis_lokal,`tb_presensi_pondokan`.`id_kelas_belajar`, `tb_presensi_pondokan`.`nama_kelas_belajar`, `tb_presensi_pondokan`.`pondokan`, `tb_presensi_pondokan`.`tingkat`, `tb_tahun_ajaran`.`tahun_ajaran` FROM `tb_pondokan_santri` inner join `tb_presensi_pondokan` on `tb_presensi_pondokan`.`id_kelas_belajar`=`tb_pondokan_santri`.`id_kelas_belajar` inner join `tb_tahun_ajaran` on `tb_tahun_ajaran`.`id_tahun`=`tb_presensi_pondokan`.`id_tahun` where `nis_lokal` = '$nis_lokal' order by `tb_presensi_pondokan`.`tingkat` asc ");
       }
 
 
