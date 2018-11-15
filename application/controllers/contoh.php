3ba6c0b9e934eaedc0ae31cabe70d5038189cd9c
M Ni'man Nasir <nikman.mnn@gmail.com>
2018-11-15 18:49:06 +0800

    versi contoh

---
+++ application/controllers/contoh.php
@@ -0,0 +1,54 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class Login extends CI_Controller{
+    function __construct(){
+		parent::__construct();
+		$this->load->model('back-end/orangtua/m_login');
+    $this->load->library('layout_pendaftaran');
+	}
+
+	function index(){
+		$this->layout_pendaftaran->renderregister('orangtua/v_login');
+  }
+
+  function ceklogin(){
+    if ($this->input->post()){
+      $kata_sandi = $this->input->post('kata_sandi', TRUE);
+      $encrypt_sandi = md5($kata_sandi);
+      $id_ortu = $this->input->post('id_ortu', TRUE);
+      $cekid = $this->m_login->cekid($id_ortu)->num_rows();
+      if ($cekid > 0){
+        $cekstatusakun =$this->m_login->cekid($id_ortu);
+        foreach ($cekstatusakun->result_array() as $akun) {
+            $statusakun = $akun['status_akun'];
+          }
+        if ($statusakun == "tidak aktif"){
+          redirect(base_url("orangtua/login?msgid=0"));
+        }
+        elseif ($statusakun == "aktif"){
+          $cek = $this->m_login->ceklogin($id_ortu, $encrypt_sandi);
+          if ($cek->num_rows() > 0) {
+            foreach ($cek->result_array() as $datauser) {
+              $sess_data['id_ortu'] = $datauser['id_ortu'];
+              $sess_data['nama_ortu'] = $datauser['nama_ortu'];
+              $sess_data['jenis_akun'] = $datauser['jenis_akun'];
+              $this->session->set_userdata($sess_data);
+            }
+            if ($cek->jenis_akun == "santri"){
+              redirect(base_url("orangtua/portalortusantri/dashboard"));
+            } else { redirect(base_url("orangtua/portalortusantriwati/dashboard"));}
+          } else { redirect(base_url("orangtua/login?msg=2"));}
+        }
+      }
+      else {
+        redirect(base_url("orangtua/login?msg=0"));
+      }
+    }
+  }
+
+  function logout(){
+        $this->session->sess_destroy();
+        redirect(base_url("orangtua/login"));
+  }
+}
+?>
